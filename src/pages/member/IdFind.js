ca929b178924655746967457590a07ff8f1a5f7a
mikada7 <mikada7@naver.com>
2025-04-26 17:57:55 +0900

    250426 작업내용 임시 커밋

---
+++ src/pages/member/IdFind.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/member/idFind.css";
+
+const IdFind = () =>{
+    return (
+        <div></div>
+    );
+}
+
+export default IdFind;
---
+++ src/pages/member/PwdCheck.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/member/pwdCheck.css";
+
+const PwdCheck = () =>{
+    return (
+        <div></div>
+    );
+}
+
+export default PwdCheck;
